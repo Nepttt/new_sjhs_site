9782cae1c1245071f7a07d07290903f0058d84d0
Nepttt <miaping111@gmail.com>
2022-11-21 23:00:45 +0900

    간식 메뉴 관련 추가

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require("express");
-const app = express();
-app.use(express.json());
+const mongoClient = require("mongodb").MongoClient;
 
-app.listen(5050, function () {
-    console.log("SJHS Website has started on port 5050");
-});
+mongoClient.connect(
+    "mongodb+srv://root:root123@cluster0.yoalkiv.mongodb.net/?retryWrites=true&w=majority",
+    function (err, client) {
+        app.listen(5050, function () {
+            console.log("SJHS Website has started on port 5050");
+        });
+    }
+);
+
+const bodyParser = require("body-parser");
+const app = express();
+const { Db } = require("mongodb");
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/snack.html", function (req, res) {
+app.get("/snack", function (req, res) {
     res.sendFile(__dirname + "/snack.html");
 });
